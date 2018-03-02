8886650e2077ecddb3469986745e5e324defe88a
Sridhar Ratnakumar <srid@srid.ca>
2018-03-02 15:13:54 -0500

    Split Types into different  modules
    And then re-export them from the global modules:
    
      - Web.Plaid (Types+Client for GHC users)
      - Web.Plaid.Types (Types only for GHCJS users)

--- plaid-client/src/Web/Plaid.hs
+++ plaid-client/src/Web/Plaid.hs
@@ -1,55 +1,7 @@
-{-# LANGUAGE DataKinds #-}
-{-# LANGUAGE LambdaCase #-}
-{-# LANGUAGE OverloadedStrings #-}
+module Web.Plaid
+  ( module Client
+  , module Types
+  ) where
 
-module Web.Plaid (
-  Environment(..),
-  getTransactions,
-  exchangeToken,
-  run,
-  module T,
-  module TC)
-  where
-
-import Data.Default.Class
-import Data.Text (Text)
-import Network.HTTP.Req
-
-import Web.Plaid.Types as T
-import Web.Plaid.Types.Core as TC
-
-plaidUrl :: Environment -> Url 'Https
-plaidUrl = \case
-  Sandbox -> https "sandbox.plaid.com"
-  Development -> https "development.plaid.com"
-  Production -> https "production.plaid.com"
-
--- | Get transactions for an item
-getTransactions :: Config -> Text -> Date -> Date -> PaginationOptions -> Req TransactionsResponse
-getTransactions config accessToken startDate endDate paginationOptions =
-  responseBody <$> req POST url body jsonResponse mempty
-    where
-      url = plaidUrl (_config_env config) /: "transactions" /: "get"
-      body = ReqBodyJson r
-      r = TransactionsRequest
-        (_config_clientId config)
-        (_config_secret config)
-        accessToken
-        startDate
-        endDate
-        paginationOptions
-
--- | Exchange the public token for access token and item id
-exchangeToken :: Config -> Text -> Req ExchangeTokenResponse
-exchangeToken config publicToken =
-  responseBody <$> req POST url body jsonResponse mempty
-    where
-      url = plaidUrl (_config_env config) /: "item" /: "public_token" /: "exchange"
-      body = ReqBodyJson r
-      r = ExchangeTokenRequest
-        (_config_clientId config)
-        (_config_secret config)
-        publicToken
-
-run :: Req a -> IO a
-run = runReq def
+import Web.Plaid.Client as Client
+import Web.Plaid.Types as Types
