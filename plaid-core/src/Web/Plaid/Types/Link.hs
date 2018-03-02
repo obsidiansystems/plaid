8ed287d5a72ccabcb50ec79869cab5833807b71b
Sridhar Ratnakumar <srid@srid.ca>
2018-03-02 16:57:41 -0500

    Remove Plaid* prefix for consistency

--- plaid-core/src/Web/Plaid/Types/Link.hs
+++ plaid-core/src/Web/Plaid/Types/Link.hs
@@ -8,39 +8,36 @@ import GHC.Generics (Generic)
 
 import Web.Plaid.Types.Core
 
--- TODO: Remove Plaid (even PlaidLink) prefix
-
-data PlaidLinkInstitution = PlaidLinkInstitution
-  { _plaidLinkInstitution_name :: !Text
-  , _plaidLinkInstitution_id :: !Text
+data LinkInstitution = LinkInstitution
+  { _linkInstitution_name :: !Text
+  , _linkInstitution_id :: !Text
   } deriving (Eq, Generic, Show)
 
-data PlaidLinkSuccess = PlaidLinkSuccess
-  { _plaidLinkSuccess_publicToken :: !Text
-  , _plaidLinkSuccess_institution :: !PlaidLinkInstitution
-  , _plaidLinkSuccess_sessionId :: !Text
+data LinkSuccess = LinkSuccess
+  { _linkSuccess_publicToken :: !Text
+  , _linkSuccess_institution :: !LinkInstitution
+  , _linkSuccess_sessionId :: !Text
   } deriving (Eq, Generic, Show)
 
-data PlaidLinkExit = PlaidLinkExit
-  { _plaidLinkExit_error :: !(Maybe PlaidLinkError)
-  , _plaidLinkExit_institution :: !(Maybe PlaidLinkInstitution)
-  , _plaidLinkExit_status :: !(Maybe Text)
-  , _plaidLinkExit_sessionId :: !Text
+data LinkExit = LinkExit
+  { _linkExit_error :: !(Maybe LinkError)
+  , _linkExit_institution :: !(Maybe LinkInstitution)
+  , _linkExit_status :: !(Maybe Text)
+  , _linkExit_sessionId :: !Text
   } deriving (Eq, Generic, Show)
 
-data PlaidLinkError = PlaidLinkError
-  { _plaidLinkError_displayMessage :: !Text
-  , _plaidLinkError_errorCode :: !Text
-  , _plaidLinkError_errorMessage :: !Text
-  , _plaidLinkError_errorType :: !Text
+data LinkError = LinkError
+  { _linkError_displayMessage :: !Text
+  , _linkError_errorCode :: !Text
+  , _linkError_errorMessage :: !Text
+  , _linkError_errorType :: !Text
   } deriving (Eq, Generic, Show)
 
--- TODO: Use Types.Config + separate products event
-data PlaidLinkConfig = PlaidLinkConfig
-  { _plaidLinkConfig_clientName :: !Text
-  , _plaidLinkConfig_env :: !Environment
-  , _plaidLinkConfig_publicKey :: !Text
-  , _plaidLinkConfig_products :: ![Product]
+data LinkConfig = LinkConfig
+  { _linkConfig_clientName :: !Text
+  , _linkConfig_env :: !Environment
+  , _linkConfig_publicKey :: !Text
+  , _linkConfig_products :: ![Product]
   } deriving (Eq, Generic, Show)
 
 -- TODO: port demo example
--- reflex-dom-plaid/Reflex/Dom/Plaid/Link.hs
+++ reflex-dom-plaid/Reflex/Dom/Plaid/Link.hs
@@ -24,21 +24,21 @@ import Web.Plaid.Types.Link
 
 plaidLinkDialog
   :: (TriggerEvent t m, PerformEvent t m, MonadJSM (Performable m))
-  => Event t PlaidLinkConfig
-  -> m (Event t (Either PlaidLinkExit PlaidLinkSuccess))
+  => Event t LinkConfig
+  -> m (Event t (Either LinkExit LinkSuccess))
 plaidLinkDialog open = do
   (onResultEvent, onResultCallback) <- newTriggerEvent
 
   performEvent_ $ ffor open $ \cfg -> liftJSM $
-    activatePlaidLinkDialog cfg (liftIO . onResultCallback)
+    activateLinkDialog cfg (liftIO . onResultCallback)
 
   pure onResultEvent
 
-activatePlaidLinkDialog
-  :: PlaidLinkConfig
-  -> (Either PlaidLinkExit PlaidLinkSuccess -> JSM ())
+activateLinkDialog
+  :: LinkConfig
+  -> (Either LinkExit LinkSuccess -> JSM ())
   -> JSM ()
-activatePlaidLinkDialog cfg onResult = do
+activateLinkDialog cfg onResult = do
   handleMVar <- liftIO newEmptyMVar
   o <- plaidLinkConfigAsObj
   o ^. jss (t_ "onSuccess") (fun $ \_ _ args -> case args of
@@ -48,10 +48,10 @@ activatePlaidLinkDialog cfg onResult = do
         Just institution -> do
           pubToken <- fromJSValUnchecked pubTokenJs
           sessionId <- maybeJs valToText =<< metaJs ^. js (t_ "link_session_id")
-          onResult $ Right PlaidLinkSuccess
-            { _plaidLinkSuccess_publicToken = pubToken
-            , _plaidLinkSuccess_institution = institution
-            , _plaidLinkSuccess_sessionId = fromMaybe "" sessionId
+          onResult $ Right LinkSuccess
+            { _linkSuccess_publicToken = pubToken
+            , _linkSuccess_institution = institution
+            , _linkSuccess_sessionId = fromMaybe "" sessionId
             }
 
     _ -> liftIO $ putStrLn "Plaid onSuccess called with unexpected number of arguments"
@@ -64,11 +64,11 @@ activatePlaidLinkDialog cfg onResult = do
       sessionId <- maybeJs valToText =<< o ^. js (t_ "link_session_id")
       status <- maybeJs valToText =<< o ^. js (t_ "status")
 
-      onResult $ Left PlaidLinkExit
-        { _plaidLinkExit_error = err
-        , _plaidLinkExit_institution = join institution
-        , _plaidLinkExit_sessionId = fromMaybe "" sessionId
-        , _plaidLinkExit_status = status
+      onResult $ Left LinkExit
+        { _linkExit_error = err
+        , _linkExit_institution = join institution
+        , _linkExit_sessionId = fromMaybe "" sessionId
+        , _linkExit_status = status
         }
 
     _ -> liftIO $ putStrLn "Plaid onExit called with unexpected number of arguments"
@@ -87,10 +87,10 @@ activatePlaidLinkDialog cfg onResult = do
   where
     plaidLinkConfigAsObj = do
       o <- obj
-      o ^. jss (t_ "clientName") (_plaidLinkConfig_clientName cfg)
-      o ^. jss (t_ "env") (plaidLinkEnvironmentAsText $ _plaidLinkConfig_env cfg)
-      o ^. jss (t_ "key") (_plaidLinkConfig_publicKey cfg)
-      o ^. jss (t_ "product") (plaidLinkProductAsText <$> _plaidLinkConfig_products cfg)
+      o ^. jss (t_ "clientName") (_linkConfig_clientName cfg)
+      o ^. jss (t_ "env") (plaidLinkEnvironmentAsText $ _linkConfig_env cfg)
+      o ^. jss (t_ "key") (_linkConfig_publicKey cfg)
+      o ^. jss (t_ "product") (plaidLinkProductAsText <$> _linkConfig_products cfg)
       pure o
 
     mkPlaidErrorFromObj o = do
@@ -98,20 +98,20 @@ activatePlaidLinkDialog cfg onResult = do
       errorCode <- valToText =<< o ^. js (t_ "error_code")
       errorMessage <- valToText =<< o ^. js (t_ "error_message")
       errorType <- valToText =<< o ^. js (t_ "error_type")
-      pure PlaidLinkError
-        { _plaidLinkError_displayMessage = displayMessage
-        , _plaidLinkError_errorCode = errorCode
-        , _plaidLinkError_errorMessage = errorMessage
-        , _plaidLinkError_errorType = errorType
+      pure LinkError
+        { _linkError_displayMessage = displayMessage
+        , _linkError_errorCode = errorCode
+        , _linkError_errorMessage = errorMessage
+        , _linkError_errorType = errorType
         }
 
     mkInstitutionFromObj o = do
       name' <- maybeJs valToText =<< o ^. js (t_ "name")
       id' <- maybeJs valToText =<< o ^. js (t_ "institution_id")
       pure $ case (name', id') of
-        (Just name, Just id_) -> Just PlaidLinkInstitution
-          { _plaidLinkInstitution_name = name
-          , _plaidLinkInstitution_id = id_
+        (Just name, Just id_) -> Just LinkInstitution
+          { _linkInstitution_name = name
+          , _linkInstitution_id = id_
           }
         _ -> Nothing
 
